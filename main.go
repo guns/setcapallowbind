7d768e858f3c58896da4640f0ef6c80b1b121a06
guns <self@sungpae.com>
2017-03-14 00:10:01 -0500

    Allow multiple file arguments

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 /*
- * Copyright (c) 2016 Sung Pae <self@sungpae.com>
+ * Copyright (c) 2016-2017 Sung Pae <self@sungpae.com>
+ * Distributed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
  */
 
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -13,15 +16,33 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
-const usagesummary = `path
+const usage = `file …
 
 Simple wrapper that calls:
 
-  /usr/bin/setcap cap_net_bind_service=+ep path
+  /usr/bin/setcap cap_net_bind_service=+ep file …
 
 Add to sudoers file:
 
   user ALL = (ALL) NOPASSWD: /path/to/setcapallowbind`
+
+type options struct {
+	Help bool `short:"h" long:"help"`
+}
+
+func validate(opts *options, args []string) error {
+	if len(args) == 0 {
+		return errors.New("nothing to do; see --help")
+	}
+
+	for i := range args {
+		if _, err := os.Stat(args[i]); os.IsNotExist(err) {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func abort(err error) {
 	if err != nil {
@@ -30,33 +51,42 @@ func abort(err error) {
 	os.Exit(1)
 }
 
-func abortWithHelp(parser *flags.Parser) {
-	parser.WriteHelp(os.Stderr)
-	os.Exit(1)
-}
-
-func getopts(arguments []string) (parser *flags.Parser, args []string) {
+func getopts(arguments []string) (opts *options, args []string) {
+	opts = new(options)
 	var err error
 
-	parser = flags.NewNamedParser(path.Base(arguments[0]), flags.HelpFlag|flags.PassDoubleDash)
-	parser.Usage = usagesummary
+	parser := flags.NewNamedParser(path.Base(arguments[0]), flags.PassDoubleDash)
+	parser.Usage = usage
+
+	if _, err = parser.AddGroup("Options", "", opts); err != nil {
+		abort(err)
+	}
 
 	if args, err = parser.ParseArgs(arguments[1:]); err != nil {
 		abort(err)
 	}
 
-	return parser, args
-}
+	if opts.Help {
+		parser.WriteHelp(os.Stderr)
+		os.Exit(0)
+	}
 
-func main() {
-	parser, args := getopts(os.Args)
-	if len(args) != 1 {
-		abortWithHelp(parser)
-	} else if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+	if err = validate(opts, args); err != nil {
 		abort(err)
 	}
 
-	if err := syscall.Exec("/usr/bin/setcap", []string{"setcap", "cap_net_bind_service=+ep", args[0]}, nil); err != nil {
+	return opts, args
+}
+
+func main() {
+	_, args := getopts(os.Args)
+	cmd := []string{"setcap"}
+
+	for i := range args {
+		cmd = append(cmd, "cap_net_bind_service=+ep", args[i])
+	}
+
+	if err := syscall.Exec("/usr/bin/setcap", cmd, nil); err != nil {
 		abort(err)
 	}
 }
